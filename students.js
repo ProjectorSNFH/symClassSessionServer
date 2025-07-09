f98ff371dbda43e257d9dfb36318a07c1d7e48f2
ProjectorSNFH <whatwhat0108@icloud.com>
2025-07-10 00:08:49 +0900

    Update students.js

--- students.js
+++ students.js
@@ -2,33 +2,173 @@
 
 const students = [
   {
-    number: 3,
-    name: "이재원",
-    id: "sym03",
-    password: "1234",
-    role: "NAS"
-  },
-  {
-    number: 1,
-    name: "김민준",
-    id: "sym01",
-    password: "abcd",
-    role: "BDM"
-  },
-  {
-    number: 2,
-    name: "박서연",
-    id: "sym02",
-    password: "qwer",
-    role: "DTM_I"
-  },
-  {
-    number: 99,
+    number: 0,
     name: "관리자",
     id: "admin",
     password: "admin123",
     role: "ADM"
-  }
+  },
+  {
+    number: 1,
+    name: "권윤서",
+    id: "sym01",
+    password: "pw01",
+    role: "DTM_A"
+  },
+  {
+    number: 2,
+    name: "김이은",
+    id: "sym02",
+    password: "pw02",
+    role: "NAS"
+  },
+  {
+    number: 3,
+    name: "박해림",
+    id: "sym03",
+    password: "pw03",
+    role: "NAS"
+  },
+  {
+    number: 4,
+    name: "이건희",
+    id: "sym04",
+    password: "pw04",
+    role: "NAS"
+  },
+  {
+    number: 5,
+    name: "이윤하",
+    id: "sym05",
+    password: "pw05",
+    role: "NAS"
+  },
+  {
+    number: 6,
+    name: "이재원",
+    id: "sym06",
+    password: "pw06",
+    role: "ADM"
+  },
+  {
+    number: 7,
+    name: "이태영",
+    id: "sym07",
+    password: "pw07",
+    role: "NAS"
+  },
+  {
+    number: 8,
+    name: "장은채",
+    id: "sym08",
+    password: "pw08",
+    role: "NAS"
+  },
+  {
+    number: 9,
+    name: "정나연",
+    id: "sym09",
+    password: "pw09",
+    role: "NAS"
+  },
+  {
+    number: 10,
+    name: "정아윤",
+    id: "sym10",
+    password: "pw10",
+    role: "NAS"
+  },
+  {
+    number: 11,
+    name: "정은혁",
+    id: "sym11",
+    password: "pw11",
+    role: "NAS"
+  },
+  {
+    number: 12,
+    name: "조재형",
+    id: "sym12",
+    password: "pw12",
+    role: "NAS"
+  },
+  {
+    number: 13,
+    name: "최다연",
+    id: "sym13",
+    password: "pw13",
+    role: "NAS"
+  },
+  {
+    number: 14,
+    name: "최연",
+    id: "sym14",
+    password: "pw14",
+    role: "NAS"
+  },
+  {
+    number: 15,
+    name: "하유민",
+    id: "sym15",
+    password: "pw15",
+    role: "NAS"
+  },
+  {
+    number: 16,
+    name: "하윤",
+    id: "sym16",
+    password: "pw16",
+    role: "NAS"
+  },
+  {
+    number: 17,
+    name: "함승아",
+    id: "sym17",
+    password: "pw17",
+    role: "NAS"
+  },
+  {
+    number: 18,
+    name: "홍유상",
+    id: "sym18",
+    password: "pw18",
+    role: "NAS"
+  },
+  {
+    number: 19,
+    name: "황샘",
+    id: "sym19",
+    password: "pw19",
+    role: "NAS"
+  },
+  {
+    number: 20,
+    name: "황윤성",
+    id: "sym20",
+    password: "pw20",
+    role: "NAS"
+  },
+  {
+    number: 21,
+    name: "황은성",
+    id: "sym21",
+    password: "pw21",
+    role: "NAS"
+  },
+  {
+    number: 22,
+    name: "황재용",
+    id: "sym22",
+    password: "pw22",
+    role: "NAS"
+  },
+  {
+    number: 23,
+    name: "황주영",
+    id: "sym23",
+    password: "pw23",
+    role: "NAS"
+  },
 ];
 
 module.exports = students;
